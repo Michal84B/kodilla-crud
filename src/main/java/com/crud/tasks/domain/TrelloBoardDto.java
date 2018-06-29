7dbc5ca346692d248496f627d4c27bd444da82aa
Michal84B <banbura.michal@gmail.com>
2018-06-29 17:26:36 +0200

    added JsonProperies

--- src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -1,14 +1,25 @@
 package com.crud.tasks.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TrelloBoardDto {
 
+    @JsonProperty("name")
     private String name;
+
+    @JsonProperty("id")
     private String id;
+
+    @JsonProperty("lists")
+    private List<TrelloListDto> lists;
 }
