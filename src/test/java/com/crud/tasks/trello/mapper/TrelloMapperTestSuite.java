17c965b01f836d00ad9c933e12884cbdbc4bd29e
Michal84B <banbura.michal@gmail.com>
2018-07-18 16:56:45 +0200

    testMapToBoards & mapToBoardsDto

---
+++ src/test/java/com/crud/tasks/trello/mapper/TrelloMapperTestSuite.java
@@ -0,0 +1,52 @@
+package com.crud.tasks.trello.mapper;
+
+import com.crud.tasks.domain.TrelloBoard;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloList;
+import com.crud.tasks.domain.TrelloListDto;
+import com.crud.tasks.mapper.TrelloMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrelloMapperTestSuite {
+    @Autowired
+    TrelloMapper trelloMapper;
+
+    @Test
+    public void testMapToBoards() {
+        //Given
+        List<TrelloBoardDto> trelloBoardDto = new ArrayList<>();
+        List<TrelloListDto> trelloListDto = new ArrayList<>();
+        trelloListDto.add(new TrelloListDto("List1", "id1", false));
+        trelloBoardDto.add(new TrelloBoardDto("Board1", "id1", trelloListDto));
+        //When
+        List<TrelloBoard> trelloBoard = trelloMapper.mapToBoards(trelloBoardDto);
+        //Then
+        assertEquals("Board1", trelloBoard.get(0).getName());
+        assertEquals(1, trelloBoard.get(0).getLists().size());
+    }
+    @Test
+    public void testMapToBoardsDto() {
+        //Given
+        List<TrelloBoard> trelloBoard = new ArrayList<>();
+        List<TrelloList> trelloList = new ArrayList<>();
+        trelloList.add(new TrelloList("id1", "List1", false));
+        trelloBoard.add(new TrelloBoard("id1", "Board1", trelloList));
+        //When
+        List<TrelloBoardDto> trelloBoardDto = trelloMapper.mapToBoardsDto(trelloBoard);
+        //Then
+        assertEquals("Board1", trelloBoardDto.get(0).getName());
+        assertEquals(1, trelloBoardDto.get(0).getLists().size());
+    }
+
+}
