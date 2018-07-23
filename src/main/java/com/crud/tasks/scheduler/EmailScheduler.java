30b8d94d30bf5e487dc9bdbe14f55ca757dd3561
Michal84B <banbura.michal@gmail.com>
2018-07-23 22:49:58 +0200

    Actuator

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -23,8 +23,8 @@ public class EmailScheduler {
 
     private static final String SUBJECT = "Tasks: Once a day email";
 
-    //@Scheduled(cron="0 0 10 * * *")
-    @Scheduled(fixedDelay = 10000)
+    @Scheduled(cron="0 0 10 * * *")
+    //@Scheduled(fixedDelay = 10000)
     public void sendInformationEmail() {
         long size = taskRepository.count();
         simpleEmailService.send(new Mail(
